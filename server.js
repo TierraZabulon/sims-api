3434d9a8f43814447eb01c16a4a719148668f933
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-06-05 21:56:20 -0400

    server update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const PORT = 8000;
 
 const simCharacters = {
-  "bob pancakes": {
+  bob: {
     name: "bob pancakes",
     gender: "male",
     age: "young adult",
@@ -12,12 +12,28 @@ const simCharacters = {
     traits: ["slob", "gloomy", "loner", "essense of flavor"],
     world: "willow creek",
   },
-  "eliza pancakes": {
+  eliza: {
     name: "eliza pancakes",
     gender: "female",
     age: "young adult",
     career: "unemployed",
     traits: ["materialistic", "neat", "perfectionist", "business savvy"],
+    world: "willow creek",
+  },
+  dina: {
+    name: "dina caliente",
+    gender: "female",
+    age: "young adult",
+    career: "assistant dishwasher",
+    traits: ["active", "ambitious", "romantic", "essense of flavor"],
+    world: "oasis springs",
+  },
+  nina: {
+    name: "nina caliente",
+    gender: "female",
+    age: "young adult",
+    career: "unemployed",
+    traits: ["lazy", "hot headed", "romantic", "dastardly"],
     world: "willow creek",
   },
 };
@@ -29,6 +45,12 @@ app.get("/", (req, res) => {
 app.get("/api/:thesims", (req, res) => {
   const sims = req.params.thesims;
   res.json(simCharacters[sims]);
+
+  if (simCharacters[sims]) {
+    res.json(simCharacters[sims]);
+  } else {
+    console.log("error");
+  }
 });
 
 app.listen(process.env.PORT || PORT, function () {
